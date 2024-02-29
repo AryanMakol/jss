1a9c8541bf8fd55a5675a22def99a0c9f76017f9
Ubuntu <jenkinsuser@JenkinsVm.prdqndjtnjpuvnhnbyd4zdycla.rx.internal.cloudapp.net>
2024-02-29 08:34:37 +0000

    added

---
+++ jenkins/TestReport/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        UserBO userBo = new UserBO();
+
+        Console.WriteLine("Enter the username");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter the password");
+        string password = Console.ReadLine();
+        Console.WriteLine("Enter the email");
+        string email = Console.ReadLine();
+
+        User user = userBo.GetUserInstance(name, password, email);
+
+        Console.WriteLine("User details");
+
+        Console.WriteLine(user);
+
+        Console.ReadKey();
+    }
+}
